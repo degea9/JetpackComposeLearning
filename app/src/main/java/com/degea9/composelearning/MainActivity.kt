d1e52796e1d49651096b08b00f101dc49b26ce2a
Tuan Dang <degea9@gmail.com>
2021-05-09 17:58:09 +0700

    Lesson 3: Material design

--- app/src/main/java/com/degea9/composelearning/MainActivity.kt
+++ app/src/main/java/com/degea9/composelearning/MainActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.degea9.composelearning.ui.theme.ComposeLearningTheme
+import com.degea9.composelearning.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,20 +32,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.header),
-            contentDescription = null,
-            modifier = Modifier.height(180.dp).fillMaxWidth(),
-            contentScale = ContentScale.Crop)
-        Spacer(Modifier.height(16.dp))
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+    MaterialTheme() {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(16.dp))
+            Text(
+                "A day wandering through the sandhills " +
+                "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = Typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text("Davenport, California",style = Typography.body2)
+            Text("December 2018",style = Typography.body2)
+        }
     }
-
 }
 
 @Preview
