63532cb649ec041988046f5bb224411fab91c1ac
Tuan Dang <degea9@gmail.com>
2021-05-09 17:51:46 +0700

    Lesson 2: Layout

--- app/src/main/java/com/degea9/composelearning/MainActivity.kt
+++ app/src/main/java/com/degea9/composelearning/MainActivity.kt
@@ -3,36 +3,50 @@ package com.degea9.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.degea9.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("test compose")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier.height(180.dp).fillMaxWidth(),
+            contentScale = ContentScale.Crop)
+        Spacer(Modifier.height(16.dp))
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
-    ComposeLearningTheme {
-        Greeting("Android.")
-    }
+    NewsStory()
 }
+
